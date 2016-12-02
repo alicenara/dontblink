4c8101fdd71fff9c76ebf1b1f0404cc0a1d3e5df
YInglai Jo <joyinglai@qq.com>
2016-12-02 14:27:07 +0100

    gameUILogic, show hint

--- DontBlink/Assets/Scripts/GameLogicUI.cs
+++ DontBlink/Assets/Scripts/GameLogicUI.cs
@@ -1,15 +1,36 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
 
 public class GameLogicUI : MonoBehaviour {
 
+	public GameObject GameStartCanvas;
+	public GameObject LookEnemyHint;
+
+	private GameObject[] enemies;
 	// Use this for initialization
 	void Start () {
-	
+		StartCoroutine (hideUI (GameStartCanvas, 3.0f)); //Wait 2 seconds then hide UI
 	}
-	
+
 	// Update is called once per frame
 	void Update () {
-	
+		enemies = GameObject.FindGameObjectsWithTag ("Enemy");
+		bool showEnemyHint = false;
+		foreach (GameObject enemy in enemies) {
+			if (!enemy.GetComponent<CheckForObservers> ().IsObserved ()) {
+				showEnemyHint = true;
+			}
+		}
+		if (showEnemyHint) {
+			LookEnemyHint.SetActive (true);
+			StartCoroutine (hideUI (LookEnemyHint, 2.0f));
+		}
+	}
+
+	IEnumerator hideUI (GameObject guiParentCanvas, float secondsToWait,bool show = false)
+	{
+		yield return new WaitForSeconds (secondsToWait);
+		guiParentCanvas.SetActive (show);
 	}
 }

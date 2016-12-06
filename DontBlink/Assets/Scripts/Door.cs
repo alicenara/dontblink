140c21787825ead1a4e06c2236c018c839c347db
oscfri <oscar.c.friberg@gmail.com>
2016-12-07 00:15:20 +0100

    Added lights over the doors

--- DontBlink/Assets/Scripts/Door.cs
+++ DontBlink/Assets/Scripts/Door.cs
@@ -7,11 +7,15 @@ public class Door : MonoBehaviour {
 	public float DoorOpenAngle = 90f;
 	public float DoorCloseAngle = 180f;
 	public float Smooth = 2f;
+	public PuzzleInteract puzzle;
 	AudioSource doorOpenSound;
+	public Light doorLight;
 
 	public void ChangeDoorState(){
-		open = !open;
-		doorOpenSound.Play ();
+		if (puzzle == null || puzzle.isSolved()) {
+			open = !open;
+			doorOpenSound.Play ();
+		}
 	}
 	// Use this for initialization
 	void Start () {
@@ -20,7 +24,15 @@ public class Door : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-
+		if (puzzle != null && !puzzle.isSolved()) {
+			if (doorLight != null) {
+				doorLight.color = Color.red;
+			}
+		} else {
+			if (doorLight != null) {
+				doorLight.color = Color.green;
+			}
+		}
 		if (open) {
 			// open the door
 			Quaternion targetRotationOpen = Quaternion.Euler (0, DoorOpenAngle, 0);

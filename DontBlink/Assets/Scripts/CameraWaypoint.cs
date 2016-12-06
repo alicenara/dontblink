e0e8fa521aa6da1f4a7b2e81f8c43490a833de76
Alice <alicenara165@gmail.com>
2016-12-06 23:36:26 +0100

    camera moving through waypoints in a new scene

---
+++ DontBlink/Assets/Scripts/CameraWaypoint.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraWaypoint : MonoBehaviour {
+
+	// Use this for initialization
+	void OnDrawGizmos () {
+        Gizmos.color = Color.cyan;
+        Gizmos.DrawSphere(transform.position, 0.1f);
+        Gizmos.color = Color.magenta;
+        Gizmos.DrawLine(transform.position, transform.position + transform.forward * 1.5f);
+    }
+}
